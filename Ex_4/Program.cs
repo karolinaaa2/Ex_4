4a2680efced0d1ac85edae1aa053c522aeade547
karolinaaa2 <hryhorchuk.karolina@chnu.edu.ua>
2024-10-05 00:35:43 +0300

    Add project files.

---
+++ Ex_4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введіть символ: ");
+        char symbol = char.ToUpper(Console.ReadLine()[0]);
+
+        if ("AEIOU".Contains(symbol))
+        {
+            Console.WriteLine("Це голосна.");
+        }
+        else
+        {
+            Console.WriteLine("Це приголосна.");
+        }
+    }
+}
